92c15382a2d78eea354377789124343c2f358542
anmc-ab1314 <bnmc.319@gmail.com>
2025-12-09 13:10:16 -0400

    views mudadas para mostrarem o conteúdo correto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('Artmaxi', 'Artmaxi') }}</title>
+        <title>{{ config('app.name', 'Artmaxi') }}</title>
         <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 
 
